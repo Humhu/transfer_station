b9d0c75c658501bd530c8b8b4d3981b2d790f03a
Dale McConachie <mcconachie@cmu.edu>
2015-06-01 19:05:49 -0400

    Updated the way that ipc_start_central finds the central executable.

--- scripts/ipc_start_central.sh
+++ scripts/ipc_start_central.sh
@@ -1,9 +1,21 @@
 #! /bin/bash
 
-if [ -e ~/boeing/ipc/bin/Linux-3.13 ]
-then
-  cd ~/boeing/ipc/bin/Linux-3.13
-fi
+HINTS=""
+HINTS+=" $HOME/boeing/ipc/bin/Linux-3.13"
+HINTS+=" $HOME/Software/ipc/bin/Linux-3.13"
+HINTS+=" $HOME/Software/ipc/bin/Linux-3.16"
 
-export CENTRALHOST=$1
-./central
+FILE="$(find ${HINTS} -name central -executable -print -quit 2> /dev/null)"
+
+echo $FILE
+
+DIR=$(dirname ${FILE})
+
+cd ${DIR}
+
+if [ ! -z "$1" ]
+then
+  export CENTRALHOST=$1
+  shift
+fi
+./central $*
