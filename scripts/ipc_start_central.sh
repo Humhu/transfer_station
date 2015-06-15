4f2a6f1881bf1bde6f2e17f336b1f83b09dede21
Dale McConachie <mcconachie@cmu.edu>
2015-06-15 15:39:43 -0400

    Updated script and launch file to properly handle args to central.

--- scripts/ipc_start_central.sh
+++ scripts/ipc_start_central.sh
@@ -7,15 +7,8 @@ HINTS+=" $HOME/Software/ipc/bin/Linux-3.16"
 
 FILE="$(find ${HINTS} -name central -executable -print -quit 2> /dev/null)"
 
-echo $FILE
-
 DIR=$(dirname ${FILE})
 
 cd ${DIR}
 
-if [ ! -z "$1" ]
-then
-  export CENTRALHOST=$1
-  shift
-fi
 ./central $*
